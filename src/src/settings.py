fc95542302e52e61d97720e6a4acbef47aa74225
parsarezaee <parsarezaee777@yahoo.com>
2022-12-06 22:08:40 +0330

    Add Order App
    Add Order Model

--- src/src/settings.py
+++ src/src/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shop.apps.ShopConfig',
-    'cart.apps.CartConfig'
+    'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig'
 ]
 
 MIDDLEWARE = [
