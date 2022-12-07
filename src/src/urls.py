61614d2705346f119be4412a89faa77ca32c8c73
parsarezaee <parsarezaee777@yahoo.com>
2022-12-07 15:21:06 +0330

    Add customer order form

--- src/src/urls.py
+++ src/src/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
-    path('', include('shop.urls', namespace='shop'))
+    path('orders/', include('orders.urls', namespace='orders')),
+    path('', include('shop.urls', namespace='shop')),
 ]
 
 
