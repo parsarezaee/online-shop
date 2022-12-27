9c67d7602b560f12f65872f963c69f81788ffd9f
parsarezaee <parsarezaee777@yahoo.com>
2022-12-27 14:17:52 +0330

    Display information about order in adminstration

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -1,8 +1,15 @@
-from django.contrib import admin
-from .models import Order, OrderItem
 import csv
 import datetime
+from django.contrib import admin
+from .models import Order, OrderItem
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -38,7 +45,7 @@ class OrderItemInline(admin.TabularInline):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address',
-    'postal_code', 'city', 'paid', 'created', 'updated']
+    'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
--- src/orders/views.py
+++ src/orders/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
-from .models import OrderItem
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
+from django.contrib.admin.views.decorators import staff_member_required
 
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 
+                  'orders/order/detail.html',
+                  {'order': order})
 
 
 def order_create(request):
