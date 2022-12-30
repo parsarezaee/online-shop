c694be8c59242299146fb05ac625a4c3e8f93621
parsarezaee <parsarezaee777@yahoo.com>
2022-12-30 20:50:31 +0330

    Display PDF link for order's information in admin panel

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -44,8 +44,12 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    def order_pdf(obj):
+        url = reverse('orders:admin_order_pdf', args=[obj.id])
+        return mark_safe(f'<a href="{url}">PDF</a>')
+    order_pdf.short_description = 'Invoice'
     list_display = ['id', 'first_name', 'last_name', 'email', 'address',
-    'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
+    'postal_code', 'city', 'paid', 'created', 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
--- src/orders/views.py
+++ src/orders/views.py
@@ -5,6 +5,11 @@ from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
 from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+import weasyprint
+
 
 
 @staff_member_required
@@ -13,6 +18,19 @@ def admin_order_detail(request, order_id):
     return render(request, 
                   'orders/order/detail.html',
                   {'order': order})
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html',
+                            {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response,
+        stylesheets=[weasyprint.CSS(
+            settings.STATIC_ROOT + 'css/pdf.css')])
+    return response
 
 
 def order_create(request):
