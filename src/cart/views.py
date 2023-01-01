56a015d288ed680376fb3a794840ddae3d36160f
parsarezaee <parsarezaee777@yahoo.com>
2023-01-02 00:18:12 +0330

    Apply coupons to orders

--- src/cart/views.py
+++ src/cart/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+from coupons.forms import CouponApplyForm
 
 
 
@@ -34,4 +35,10 @@ def cart_detail(request):
             'quantify': item['quantify'],
             'override': True
         })
-    return render(request, 'cart/detail.html', {'cart': cart})
+    
+    coupon_apply_form = CouponApplyForm()
+
+    return render(request, 
+                  'cart/detail.html',
+                  {'cart': cart,
+                  'coupon_apply_form': coupon_apply_form})
