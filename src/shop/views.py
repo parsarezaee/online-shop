8285b2987931d63cfdcf5398d9808f393a9b41f7
parsarezaee <parsarezaee777@yahoo.com>
2023-01-05 16:14:59 +0330

    Create views for translations

--- src/shop/views.py
+++ src/shop/views.py
@@ -9,7 +9,10 @@ def product_list(request, category_slug=None):
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
     if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
+        language = request.LANGUAGE_CODE
+        category = get_object_or_404(Category, 
+                                    translations__language_code = language,
+                                    translations__slug = category_slug)
         products = products.filter(category=category)
     return render(request, 'shop/product/list.html',
         {'category': category,
@@ -18,10 +21,12 @@ def product_list(request, category_slug=None):
 
 
 def product_detail(request, id, slug):
+    language = request.LANGUAGE_CODE
     product = get_object_or_404(
         Product,
         id=id,
-        slug=slug,
+        translations__language_code = language,
+        translations__slug = slug,
         available=True
     )
     cart_product_form = CartAddProductForm()
